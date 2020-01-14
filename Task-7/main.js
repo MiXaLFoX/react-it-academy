2df347c04576112a87fdcf32002d5b6e9b4dcfaf
Mikhail Lis <mixal8411@gmail.com>
2020-01-14 12:24:29 +0300

    refactor(task7): change sort, remove reverse

--- Task-7/main.js
+++ Task-7/main.js
@@ -1,7 +1,8 @@
 function largestN (arr, num) {
-  return arr.sort().reverse()[num - 1];
+  return arr.sort((a, b) => b - a)[num - 1];
 }
 
 console.log(largestN([2, 4, 1, 5, 3], 1)) // 5
 console.log(largestN([2, 4, 1, 5, 3], 2)) // 4
 console.log(largestN([1, 8, 3, 2], 4)) // 1
+console.log(largestN([1, 2, 15], 1))
