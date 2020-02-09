d59ab7c7351e1b9f1a39cd3a73771ca7c3669d10
Mikhail Lis <mixal8411@gmail.com>
2020-02-10 00:47:08 +0300

    refactor(react-counters): change handler, trying to make sum of the counters

--- src/App.js
+++ src/App.js
@@ -3,68 +3,47 @@ import React from 'react';
 class Counter extends React.Component {
   constructor (props) {
     super (props);
-    this.incr = props.data.increment || 1;
-    this.decr = props.data.decrement || 1;
-    this.max = props.data.maxRange || 10;
-    this.min = props.data.minRange || -10;
     this.state = {
-      count: 0
+      count: this.props.options.initial
     };
-    this.handleIncrement = this.handleIncrement.bind(this);
-    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleIncrement () {
-    this.setState({
-      count: this.state.count + this.incr
-    });
-    if (this.state.count >= this.max) {
-      this.setState({
-        count: this.state.count
-      })
-    }
+  isNumValid (num) {
+    return num >= this.props.options.range.min && num <= this.props.options.range.max;
   }
 
-  handleDecrement () {
-    this.setState({
-      count: this.state.count - this.decr
-    });
-    if (this.state.count <= this.min) {
-      this.setState({
-        count: this.state.count
-      })
+  handleClick ({currentTarget}) {
+    const {step, range: {min, max}} = this.props.options;
+    const operation = currentTarget.getAttribute('data-operation');
+    let newCount = this.state.count;
+    if (operation === 'incr') {
+      newCount = newCount + step;
+    } else {
+      newCount = newCount - step;
     }
+
+    if (this.isNumValid(newCount)) {
+      this.setState({
+        count: newCount
+      });
+    } else {
+      this.setState({
+        count: operation === 'incr' ? max : min
+      });
+    }
+    this.props.onChange(this.state.count);
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.handleIncrement} onClick={() => { this.props.updateData(this.state.sum)}}> + </button>
+        <button onClick={this.handleClick} data-operation="incr"> + </button>
         <span> {this.state.count} </span>
-        <button onClick={this.handleDecrement}> - </button>
+        <button onClick={this.handleClick} data-operation="decr"> - </button>
       </div>
     );
   }
 }
 
-class SuperCount extends React.Component {
-  state = {
-    sum: 0
-  };
-
-  updateData = (value) => {
-    this.setState({ sum: value })
-  };
-
-  render() {
-    return (
-    <div>
-      <p>counters sum: {this.state}</p>
-      <Counter updateData={this.updateData} data={{increment: 10, decrement: 10, maxRange: 100, minRange: -100}}/>
-      <Counter updateData={this.updateData} data={{}}/>
-    </div>
-    )
-  }
-}
-
-export default SuperCount;
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import SuperCount from './App';
+import Counter from './App';
 import * as serviceWorker from './serviceWorker';
 
+const defaultOptions = [
+  {
+    initial: 0,
+    step: 10,
+    range: {
+      min: -10,
+      max: 20
+    }
+  },
+  {
+    initial: 0,
+    step: 1,
+    range: {
+      min: -10,
+      max: 10
+    }
+  },
+  {
+    initial: 0,
+    step: 5,
+    range: {
+      min: -100,
+      max: 20
+    }
+  }
+];
+
+const handleChange = (val, idx) => {
+
+}
+
 ReactDOM.render(
-  <SuperCount />,
+  <div>
+    {defaultOptions.map((options, idx) => <Counter key={idx} options={options} onChange={(val) => this.handleChange(val, idx)} />)}
+    <div>Sum: {this.state.overalSum}</div>
+  </div>,
   document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
